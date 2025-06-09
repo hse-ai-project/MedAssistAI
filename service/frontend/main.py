33d1d7814a67dadaade5183eb15850d18d6d329c
KL\Chernykh_I <ivan.chernykh@kaspersky.com>
2025-06-10 00:01:46 +0300

    fix streamlit

--- service/frontend/main.py
+++ service/frontend/main.py
@@ -74,7 +74,7 @@ def process_main_page():
             try:
                 logger.debug("'Prompt' request has been sent")
                 user_prompt_result = requests.post(
-                    "http://127.0.0.1:8000/model/predict_from_text",
+                    "http://fastapi:8000/model/predict_from_text",
                     json=prompt_json_data,
                     timeout=10,
                 ).json()
@@ -175,7 +175,7 @@ def process_main_page():
                 try:
                     logger.debug("'Fit' request has been sent")
                     fitting_result = requests.post(
-                        "http://127.0.0.1:8000/model/fit",
+                        "http://fastapi:8000/model/fit",
                         json=fit_json_data,
                         timeout=10
                     ).json()
@@ -216,7 +216,7 @@ def process_main_page():
             try:
                 logger.debug("'Set model' request has been sent")
                 set_model_result = requests.post(
-                    "http://127.0.0.1:8000/model/set_model",
+                    "http://fastapi:8000/model/set_model",
                     json=set_json_data,
                     timeout=10,
                 ).json()
@@ -293,7 +293,7 @@ def process_main_page():
                 try:
                     logger.debug("'Update model' request has been sent")
                     update_model_result = requests.post(
-                        f"http://127.0.0.1:8000/model/update_model/{model_id}",
+                        f"http://fastapi:8000/model/update_model/{model_id}",
                         json=update_json_data,
                         timeout=10,
                     ).json()
@@ -343,7 +343,7 @@ def process_main_page():
             try:
                 logger.debug("'Predict' request has been sent")
                 predict_model_result = requests.post(
-                    "http://127.0.0.1:8000/model/predict",
+                    "http://fastapi:8000/model/predict",
                     json=predict_json_data,
                     timeout=10,
                 ).json()
@@ -400,7 +400,7 @@ def process_main_page():
                     "'Predict' (for predict probability) request has been sent"
                 )
                 predict_proba_model_result = requests.post(
-                    "http://127.0.0.1:8000/model/predict",
+                    "http://fastapi:8000/model/predict",
                     json=predict_json_data,
                     timeout=10,
                 ).json()
@@ -444,7 +444,7 @@ def process_main_page():
             try:
                 logger.debug("'Models' request has been sent")
                 models_result = requests.get(
-                    "http://127.0.0.1:8000/model/models", timeout=10
+                    "http://fastapi:8000/model/models", timeout=10
                 ).json()
                 logger.debug("Got response from API for /models")
                 if "models" in models_result:
@@ -472,7 +472,7 @@ def process_main_page():
             try:
                 logger.debug("'Participants' request has been sent")
                 participants_result = requests.get(
-                    "http://127.0.0.1:8000/participants", timeout=10
+                    "http://fastapi:8000/participants", timeout=10
                 ).json()["status"]
                 logger.debug("Got response from API for /participants")
                 write_participants(participants_result)
@@ -488,7 +488,7 @@ def process_main_page():
                 # закомментить эту строчку
 
 
-# Ниже представлены функции для отрисовки полученных данных от 127.0.0.1
+# Ниже представлены функции для отрисовки полученных данных от fastapi
 def write_models(models):
     """Отображение доступных моделей"""
     st.write("## Доступные модели")
